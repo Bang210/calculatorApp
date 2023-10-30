29ea2194b0a5d5eafb0ef339165d7643188cea3b
Bang210 <bangvelope@gmail.com>
2023-10-30 13:25:45 +0900

    사칙연산 리팩토링

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,41 +1,37 @@
 package com.ll;
 
 class Calc {
+    static String rgx;
+    static int[] expInt = new int[2];
     static int run(String exp) {
-        String rgx;
-        int[] expInt = new int[2];
+
 
         if (exp.contains("-")) {
-            rgx = "\\-";
-            String[] expString = exp.split(rgx);
-            expInt[0] = Integer.parseInt(expString[0].strip());
-            expInt[1] = Integer.parseInt(expString[1].strip());
+            assign(exp, "\\-");
             return expInt[0] - expInt[1];
         }
 
         else if (exp.contains("+")) {
-            rgx = "\\+";
-            String[] expString = exp.split(rgx);
-            expInt[0] = Integer.parseInt(expString[0].strip());
-            expInt[1] = Integer.parseInt(expString[1].strip());
+            assign(exp, "\\+");
             return expInt[0] + expInt[1];
         }
 
         else if (exp.contains("*")) {
-            rgx = "\\*";
-            String[] expString = exp.split(rgx);
-            expInt[0] = Integer.parseInt(expString[0].strip());
-            expInt[1] = Integer.parseInt(expString[1].strip());
+            assign(exp, "\\*");
             return expInt[0] * expInt[1];
         }
 
         else if (exp.contains("/")) {
-            rgx = "/";
-            String[] expString = exp.split(rgx);
-            expInt[0] = Integer.parseInt(expString[0].strip());
-            expInt[1] = Integer.parseInt(expString[1].strip());
+            assign(exp, "/");
             return expInt[0] / expInt[1];
         }
         return 0;
+    }
+
+    static int[] assign(String exp, String rgx) {
+        String[] expString = exp.split(rgx);
+        expInt[0] = Integer.parseInt(expString[0].strip());
+        expInt[1] = Integer.parseInt(expString[1].strip());
+        return expInt;
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,8 +2,8 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        //1단계(3)
-        //Calc.run("3 * 1");
+        //1단계(3) 통과
+        System.out.println(Calc.run("3 * 1"));
         //2단계(3)
         //"3 * 1 + (1 - 1)")
         //3단계(0)
