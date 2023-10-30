4a0c68cc9cdfa406b2e1c6b60b7db88f300f094f
Bang210 <bangvelope@gmail.com>
2023-10-30 11:52:42 +0900

    단항식 테스트케이스를 통해 단항식 사칙연산 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,41 @@
 package com.ll;
 
 class Calc {
-    static int run(String content) {
-        return 0;
+    static int run(String exp) {
+        String rgx;
+        int[] expInt = new int[2];
+
+        if (exp.contains("-")) {
+            rgx = "\\-";
+            String[] expString = exp.split(rgx);
+            expInt[0] = Integer.parseInt(expString[0].strip());
+            expInt[1] = Integer.parseInt(expString[1].strip());
+            return expInt[0] - expInt[1];
         }
+
+        else if (exp.contains("+")) {
+            rgx = "\\+";
+            String[] expString = exp.split(rgx);
+            expInt[0] = Integer.parseInt(expString[0].strip());
+            expInt[1] = Integer.parseInt(expString[1].strip());
+            return expInt[0] + expInt[1];
+        }
+
+        else if (exp.contains("*")) {
+            rgx = "\\*";
+            String[] expString = exp.split(rgx);
+            expInt[0] = Integer.parseInt(expString[0].strip());
+            expInt[1] = Integer.parseInt(expString[1].strip());
+            return expInt[0] * expInt[1];
+        }
+
+        else if (exp.contains("/")) {
+            rgx = "/";
+            String[] expString = exp.split(rgx);
+            expInt[0] = Integer.parseInt(expString[0].strip());
+            expInt[1] = Integer.parseInt(expString[1].strip());
+            return expInt[0] / expInt[1];
+        }
+        return 0;
     }
+}
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -3,10 +3,62 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalcTest {
     @Test
     @DisplayName("Calc의 run메서드는 int를 리턴한다.")
     void t1() {
         int rs = Calc.run("abcd");
     }
+
+    @Test
+    @DisplayName("10 + 10 = 20")
+    void t2() {
+        int rs  = Calc.run("10 + 10");
+        assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 = 0")
+    void t3() {
+        int rs  = Calc.run("10 - 10");
+        assertThat(rs).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("10 * 10 = 100")
+    void t4() {
+        int rs  = Calc.run("10 * 10");
+        assertThat(rs).isEqualTo(100);
+    }
+
+    @Test
+    @DisplayName("10 / 10 = 1")
+    void t5() {
+        int rs  = Calc.run("10 / 10");
+        assertThat(rs).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("10 + 20  = 30")
+    void t6() {
+        int rs  = Calc.run("10 + 20");
+        assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("20 - 10  = 10")
+    void t7() {
+        int rs  = Calc.run("20 - 10");
+        assertThat(rs).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("100 / 10  = 10")
+    void t8() {
+        int rs  = Calc.run("100 / 10");
+        assertThat(rs).isEqualTo(10);
+    }
+
 }
